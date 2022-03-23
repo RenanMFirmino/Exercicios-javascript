37665fb999692770e847da7adf3f6de0173496df
unknown <renanxmf@outlook.com>
2022-03-23 18:22:38 -0300

    Começo

---
+++ script.js
@@ -0,0 +1,55 @@
+// let nome =prompt();
+// console.log(nome);
+
+// let num1= parseInt(prompt("Digite um numero:"));
+// let num2= parseInt(prompt("Digite um numero:"));
+// console.log(num1 +num2);
+// console.log(num1 -num2);
+// console.log(num1 *num2);
+// console.log(num1 /num2);
+
+// let nome = prompt("Escreva seu nome:");
+
+// alert("Seu nome eh "+nome);
+// console.log(nome);
+// console.log(alert);
+
+// let num1 = parseInt(prompt("Digite um numero:"));
+// let num2 = parseInt(prompt("Digite um numero:"));
+// console.log("A soma dos numeros eh: " (num1 + num2));
+
+// let nota1 = parseFloat(prompt("Digite a primeira nota:"));
+// let nota2 = parseFloat(prompt("Digite a segunda nota:"));
+// console.log("A media eh : " +(nota1 + nota2)/2);
+
+// let anoNasci = parseInt(prompt("Digite seu ano de Nascimento:"));
+// dataAtual = new Date();
+// anoAtual= dataAtual.getFullYear();
+// console.log(anoAtual - anoNasci)
+
+// let num1 = parseInt(prompt("Digite um numero:"));
+// if (num1%2===0) { (o uso de  tres =(===) compara inclusive o tipo)
+//     console.log("numero eh par");
+// }
+// else{
+//     console.log("numero eh impar");
+// }
+
+// let num1 = parseInt(prompt("Digite um numero:"));
+// if (num1>=0) { 
+//     console.log("numero eh positivo");
+// }
+// else{
+//     console.log("numero eh negativo");
+// }
+
+let letra=prompt("Digite F(female) ou M(male):").toLowerCase();
+
+if (letra ==="f") {
+    console.log("Feminino");
+}
+else if (letra ==="m") {
+    console.log("Masculino");
+} else {
+    console.log("Sexo invalido");
+}
